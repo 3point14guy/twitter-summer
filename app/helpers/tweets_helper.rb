bb1f7aa040cbf57a7e192c3b27179b3c7dcfeab8
3poitn14guy <nicholas.piaskoski@gmail.com>
2018-08-13 09:34:15 -0400

    add work to date

---
+++ app/helpers/tweets_helper.rb
@@ -0,0 +1,26 @@
+module TweetsHelper
+
+def get_tagged(tweet)
+	message_arr = @tweet.message.split
+
+    message_arr.each_with_index do |word, index|
+      if word[0] == "#"
+        #greate a new instance of Tag
+        if Tag.pluck(:phrase).include?(word.downcase)
+          #save that Tag as a variable
+          tag = Tag.find_by(phrase: word.downcase)
+        else
+          #create a new instance of Tag
+          tag = Tag.create(phrase: word.downcase)
+        end
+
+        tweet_tag = TweetTag.create(tweet_id: @tweet.id, tag_id: tag.id)
+
+        message_arr[index] = "<a href='/tag_tweets?id=#{tag.id}'>#{word}</a>"
+      end
+    end
+
+      @tweet.update(message: message_arr.join(" "))
+      return tweet
+    end
+end
